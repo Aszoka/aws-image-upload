56e0f6847ef2fea225fc883beda7ebfd3c91b18e
Alexandra Hajdu <hajdu.alexandra09@gmail.com>
2021-11-10 13:01:13 +0100

    amazon credentials configurations

---
+++ src/main/java/com/aszoka/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,29 @@
+package com.aszoka.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    // this comes from the aws dependency
+    @Bean
+    public AmazonS3 s3() {
+        //interface
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                // this comes from the downloaded file from the aws account credentials
+                System.getenv("ACCESSKEY"),
+                System.getenv("SECRETKEY")
+        );
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
